fcca5ab21553fe1045de9b7209d6818f4c76a785
JRLopezFarfan <lopezfarfan@comunidad.unam.mx>
2023-05-22 20:16:59 -0600

    Skills1

--- TurnBasedCombat-master/Assets/Scripts/Fighters/PlayerFighter.cs
+++ TurnBasedCombat-master/Assets/Scripts/Fighters/PlayerFighter.cs
@@ -10,7 +10,7 @@ public class PlayerFighter : Fighter
 
     void Awake()
     {
-        this.stats = new Stats(21, 60, 50, 45, 20, 20);
+        this.stats = new Stats(22, 60, 50, 45, 20, 20);
     }
 
     public override void InitTurn()
